1dbd780eabdae6715f0eea91d633818282a2d800
jenny-han <dev.jennyhan@gmail.com>
2023-09-07 00:56:38 +0900

    init

--- web.js
+++ web.js
@@ -1,5 +1,20 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(8001);
+const express = require('express')
+const path = require('path');
+
+const app = express()
+const PORT = 8001
+
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, '/build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.use(express.json());
+var cors = require('cors');
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`server started on PORT ${PORT}`)
+})
