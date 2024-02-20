a15c42d32bd550f01166244f594b2c07b832795c
jenny-han <dev.jennyhan@gmail.com>
2024-02-21 03:06:17 +0900

    eslint change

--- web.js
+++ web.js
@@ -1,25 +1,23 @@
-const express = require('express')
-const path = require('path');
+const express = require("express")
+const path = require("path")
 
 const app = express()
 const PORT = 8001
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+const cors = require("cors")
+app.use(cors())
 
-const cors = require('cors');
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : false }))
+app.use(express.static(path.join(__dirname, "/client/build")))
 
-app.use(cors());
-
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // res.sendFile('index.html')
-  res.sendFile(path.join(__dirname,'/client/build/index.html'))
+  res.sendFile(path.join(__dirname, "/client/build/index.html"))
 })
 
 app.listen(PORT, () => {
-    console.log(`server started on PORT ${PORT}`)
+  console.log(`server started on PORT ${PORT}`)
 })
