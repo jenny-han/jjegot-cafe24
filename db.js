3cbabda9c47d372c82a0f3f4ef583dee75f3d840
jenny-han <dev.jennyhan@gmail.com>
2024-02-28 17:58:41 +0900

    fix file change

--- db.js
+++ db.js
@@ -1,15 +1,22 @@
 const mysql = require("mysql")
 
-console.log(process.env.REACT_APP_MYSQL_SERVER_HOST)
+// console.log(process.env.REACT_APP_MYSQL_SERVER_HOST)
 // MySQL 연결
+// const connection = mysql.createConnection({
+//   host: process.env.REACT_APP_MYSQL_SERVER_HOST,
+//   database: process.env.REACT_APP_MYSQL_DATABASE,
+//   user: process.env.REACT_APP_MYSQL_USER_NAME,
+//   password: process.env.REACT_APP_MYSQL_PASSWORD,
+//   port: 3306,
+// })
+// MySQL 연결 production
 const connection = mysql.createConnection({
-  host: process.env.REACT_APP_MYSQL_SERVER_HOST,
-  database: process.env.REACT_APP_MYSQL_DATABASE,
-  user: process.env.REACT_APP_MYSQL_USER_NAME,
-  password: process.env.REACT_APP_MYSQL_PASSWORD,
+  host: "jjegotseller.cafe24app.com",
+  database: "jjegot",
+  user: "jjegot",
+  password: "jajae2got@",
   port: 3306,
 })
-
 // MySQL 연결 테스트
 connection.connect((err) => {
   if (err) {
